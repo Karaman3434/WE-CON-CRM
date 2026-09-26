587a0ee3276980b2393e0c23f5a194a14a26bb71
Karaman3434 <akaraman3406@gmail.com>
2026-09-26 05:52:23 +0300

    Add files via upload

--- ayarlar-render.js
+++ ayarlar-render.js
@@ -1,6 +1,6 @@
 // Tek merkezi sürüm bilgisi — home.html içindeki #versiyonEtiketi ile
 // senkron tutulmalıdır. Format: WG.(GGAAYY).(SSDD).(sıra no)
-var APP_VERSION = "WG.260926.0539.635";
+var APP_VERSION = "WG.260926.0549.636";
 
 function hataGoster(mesaj){
   console.error(mesaj);
